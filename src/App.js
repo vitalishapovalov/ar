2b17808696ea28926241821247aed2213723e627
Vitali Shapovalov <shapovalov.vitali.v@gmail.com>
2018-01-31 21:45:52 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { AFrameRenderer, Marker } from 'react-web-ar';
+
+/**
+ * webcam is the default source type for analysing the marker
+ */
+class ReactArApp extends Component {
+  render() {
+    return (
+      <AFrameRenderer inherent={true}>
+        <Marker parameters={{ preset: "hiro" }}>
+          <a-box color="blue" position="0 0.09 0" scale="0.4 0.8 0.8">
+            <a-animation
+              attribute="rotation"
+              to="360 0 0"
+              dur="2000"
+              easing="linear"
+              repeat="indefinite"
+            />
+          </a-box>
+        </Marker>
+      </AFrameRenderer>
+    );
+  }
+}
+
+export default ReactArApp;
